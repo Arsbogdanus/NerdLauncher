1e005f2457b01f43e3d43d69bf2aecf42780e0fc
Bogdan Zhuravlov <arsbogdanus@gmail.com>
2024-07-22 13:50:32 +0300

    second commit

---
+++ app/src/main/java/com/brz/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,26 @@
+package com.brz.nerdlauncher;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NerdLauncherFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    public static NerdLauncherFragment newInstance() {
+        return new NerdLauncherFragment();
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_nerd_launcher, container,
+            false);
+        mRecyclerView = (RecyclerView) v.findViewById(ТR.id.app_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return v;
+    }
+}
